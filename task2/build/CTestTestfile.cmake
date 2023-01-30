35d8046220fe8adb4b027717373b270030d91251
Iostreamisolate <93423806+Iostreamisolate@users.noreply.github.com>
2023-01-30 12:05:46 +0800

    Add files via upload

---
+++ task2/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/zxy/task2/src
+# Build directory: /home/zxy/task2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("task2_pkg")
