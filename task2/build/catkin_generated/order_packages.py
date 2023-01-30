9734915fa2eb9742730ff0f5498bb1c64679b511
Iostreamisolate <93423806+Iostreamisolate@users.noreply.github.com>
2023-01-30 11:54:19 +0800

    Add files via upload

---
+++ task2/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/zxy/task2/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/zxy/task2/devel;/home/zxy/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/zxy/task2/devel;/home/zxy/catkin_ws/devel;/opt/ros/noetic' != '' else []
